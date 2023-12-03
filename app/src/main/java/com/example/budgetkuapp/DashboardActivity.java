a0cdf29f91da30267e9637052c29b804d0f13912
Rastra Wardana Nanditama <rastra@upi.edu>
2023-12-03 14:21:04 +0700

    update code

--- app/src/main/java/com/example/budgetkuapp/DashboardActivity.java
+++ app/src/main/java/com/example/budgetkuapp/DashboardActivity.java
@@ -1,14 +1,75 @@
 package com.example.budgetkuapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class DashboardActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Toolbar toolbar=findViewById(R.id.app_toolbar);
+        toolbar.setTitle("BudgetKu");
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle drawer_toggle=new ActionBarDrawerToggle(
+                this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(drawer_toggle);
+        drawer_toggle.syncState();
+
+        NavigationView navigationView=findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
+        if(drawerLayout.isDrawerOpen(GravityCompat.END)){
+            drawerLayout.closeDrawer(GravityCompat.END);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    public void displaySelectedListener(int itemId){
+        Fragment fragment=null;
+        switch(itemId){
+            case R.id.dashboard:
+            break;
+            case R.id.pengeluaran:
+            break;
+        }
+        if(fragment!=null){
+            FragmentTransaction ft= getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.main_frame,fragment);
+            ft.commit();
+        }
+        DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        displaySelectedListener(item.getItemId());
+        return true;
     }
 }
